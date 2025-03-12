f7a0ba81778caec52d9942d797aba557a1ebfb5c
yangfeng <yangfeng@kylinsec.com.cn>
2025-03-12 16:27:05 +0800

    feature(common): add notify function
    - 增加通知功能
    Related #74315

---
+++ lib/common/notify.h
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2023 ~ 2024 KylinSec Co., Ltd.
+ * kiran-shell is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ *
+ * Author:     yangfeng <yangfeng@kylinsec.com.cn>
+ */
+
+#pragma once
+
+#include <QString>
+
+namespace Kiran
+{
+namespace Common
+{
+void generalNotify(const QString& summary, const QString& body = "", const QString& icon = "");
+
+}  // namespace Common
+}  // namespace Kiran
