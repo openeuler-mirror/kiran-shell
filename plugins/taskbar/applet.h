03ae40cce2d4be4b1efebba18b508464e353ac46
yangfeng <yangfeng@kylinsec.com.cn>
2025-07-29 16:14:30 +0800

    fix(taskbar):Support the change of the 'SkipTaskbar' flag during the running of the window
    - 支持窗口运行过程中'SkipTaskbar'标志变化
    Related #89407

--- plugins/taskbar/applet.h
+++ plugins/taskbar/applet.h
@@ -36,6 +36,7 @@ signals:
     void windowAdded(WId wid);
     void windowRemoved(WId wid);
     void activeWindowChanged(WId wid);
+    void windowChanged(WId, NET::Properties, NET::Properties2);
 
 private:
     IAppletImport *m_import;
