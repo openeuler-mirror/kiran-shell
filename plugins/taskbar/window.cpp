a6fdea2f0ce7315c02641b763d2bb34babd710fc
liuxinhao <liuxinhao@kylinsec.com.cn>
2025-07-31 19:31:49 +0800

    fix(window-manager): Fix Forwarding of window deletion signal failed due to ineffective IsSkipTaskbar check during window deletion
    - 修复窗口删除时判断IsSkipTaskbar失效导致窗口删除信号未被转发, WindowManager集中实现WindowChanged事件过滤

--- plugins/taskbar/window.cpp
+++ plugins/taskbar/window.cpp
@@ -107,37 +107,7 @@ void Window::initWindowManager()
 {
     connect((Applet *)parent(), &Applet::windowAdded, this, &Window::addWindow);
     connect((Applet *)parent(), &Applet::windowRemoved, this, &Window::removeWindow);
-    connect((Applet *)parent(), &Applet::windowChanged, [this](WId wid, NET::Properties properties, NET::Properties2 properties2)
-            {
-                // 窗口打开后，运行过程中标志变更
-                bool hasContains = false;
-                for (auto &appGroups : m_mapAppGroupOpened)
-                {
-                    if (appGroups.second.contains(wid))
-                    {
-                        hasContains = true;
-                        break;
-                    }
-                }
-                if (!hasContains)
-                {
-                    if (!WindowInfoHelper::isSkipTaskbar(wid))
-                    {
-                        addWindow(wid);
-                        return;
-                    }
-                }
-                else
-                {
-                    if (WindowInfoHelper::isSkipTaskbar(wid))
-                    {
-                        removeWindow(wid);
-                        return;
-                    }
-                }
-
-                emit windowChanged(wid, properties, properties2);
-            });
+    connect((Applet *)parent(), &Applet::windowChanged, this, &Window::windowChanged);
     connect((Applet *)parent(), &Applet::activeWindowChanged, [this](WId wid)
             {
                 static WId lastWid = 0;
